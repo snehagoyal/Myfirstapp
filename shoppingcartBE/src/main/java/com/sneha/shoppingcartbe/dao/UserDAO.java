161cd71e2f95e02541b855092aae7c4ddbd1a77f
snehagoyal <snehagoyal2016@gmail.com>
2017-05-27 17:07:23 +0530

    new project
    social sites

--- shoppingcartBE/src/main/java/com/sneha/shoppingcartbe/dao/UserDAO.java
+++
@@ -1,8 +0,0 @@
-package com.sneha.shoppingcartbe.dao;
-
-import com.sneha.shoppingcartbe.model.User;
-
-public interface UserDAO {
-public void addUser(User u);
-public boolean validate(String uname, String password);
-}
--- shoppingcartBE/src/main/java/com/sneha/shoppingcartbe/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.sneha.shoppingcartbe.service;
-
-import com.sneha.shoppingcartbe.model.User;
-
-public interface UserService {
-public 	void addUser(User u);
-public boolean validate(String uname, String password);
-
-}
